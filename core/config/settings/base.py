ffee3baa035deba4e6dc2dd47748e179394a983b
Hossein Naseri <dev.h.naseri@gmail.com>
2026-09-17 13:49:16 +0330

    🔧 chore(django): add user custom model to config

--- core/config/settings/base.py
+++ core/config/settings/base.py
@@ -140,3 +140,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Custom user model
+AUTH_USER_MODEL = "users.User"
